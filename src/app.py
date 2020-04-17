8aa6682fce6f233fb84fcc4ce9ecd1a23f1d41de
Daniel Brown <daniel.brown@peakon.com>
2020-04-17 16:12:52 +0200

    update app after testing, and move py files to src folder

--- src/app.py
+++ src/app.py
@@ -3,11 +3,13 @@ from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 from utils import tidy_input, parse_input
-from read_data import read_data
+from read_data import read_data_from_db
+
 
 app = Flask(__name__)
-port = int(os.environ.get("PORT", 5000))
+# port = int(os.environ.get("PORT", 5000))
 # app.config.from_object(os.environ['APP_SETTINGS'])
+
 app.config.from_object("config.DevelopmentConfig")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -21,18 +23,15 @@ def index():
 
     input = request.args.get('myInput', None, type=str)
     app.logger.info('Input: {}'.format(input))
-    # if input is not None:
-    #     input_tidy = tidy_input(input)
-    #     input_parsed = parse_input(input_tidy)
-    #     output = read_data(app, db, input_parsed)
-    #
-    #     return render_template('template.html', text=output, value=input)
+    if input is not None:
+        input_tidy = tidy_input(input)
+        input_parsed = parse_input(input_tidy)
+        output = read_data_from_db(app, db, input_parsed)
+
+        return render_template('template.html', text=input_parsed, value=input)
 
     return render_template('template.html', text='', value="")
 
 
-
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- src/read_data.py
+++ src/read_data.py
@@ -1,6 +1,6 @@
-from models import RollModel, RollDetailModel
+# from models import RollModel, RollDetailModel
 
-def read_data(app, db, parsed_input):
+def read_data_from_db(app, db, parsed_input):
     """ """
 
     if parsed_input['kh_mod'] is None:
