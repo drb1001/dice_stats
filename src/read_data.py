61cde667548d50d6711ffc9566914a8b18035e7c
Daniel Brown <daniel.brown@peakon.com>
2020-05-18 19:17:25 +0200

    Simplify data read

--- src/read_data.py
+++ src/read_data.py
@@ -4,20 +4,9 @@ from flask import jsonify
 def read_data_from_db(app, db, Model, parsed_input):
     """ """
 
-    if parsed_input['const_sign'] == '+':
-        const_mod = int(parsed_input['const'])
-    elif parsed_input['const_sign'] == '-':
-            const_mod = -1 * int(parsed_input['const'])
-    else:
-        const_mod = 0
+    rolls = (Model.query
+            .filter_by(ndx_text=parsed_input['input_no_const'])
+            .all()
+    )
 
-
-    if parsed_input['kh_mod'] is None:
-
-        rolls = (Model.query
-                .filter_by(dice_total=parsed_input['num'])
-                .filter_by(side_count=parsed_input['sides'])
-                .all()
-        )
-
-        return [{'pip_total': r.pip_total + const_mod, 'probability': r.probability} for r in rolls]
+    return [{'pip_total': r.pip_total + parsed_input['const'], 'probability': r.probability} for r in rolls]
