5a8fe0c7e7b3067e0a145a93ee783ed7d2dc89f5
drb1001 <pi_anyone@hotmail.com>
2020-05-28 23:34:08 +0200

    Rebuild roll model - add roll_type, improve read_data

--- src/read_data.py
+++ src/read_data.py
@@ -1,12 +1,29 @@
-from flask import jsonify
-
 
 def read_data_from_db(app, db, Model, parsed_input):
     """ """
 
-    rolls = (Model.query
-            .filter_by(ndx_text=parsed_input['input_no_const'])
-            .all()
-    )
+    if parsed_input['kh_mod'] is not None:
+        rolls = (Model.query
+                .filter_by(dice_total=parsed_input['num'])
+                .filter_by(side_count=parsed_input['sides'])
+                .filter_by(roll_type=parsed_input['kh_mod'])
+                .all()
+        )
+
+    elif parsed_input['r_mod'] in ['r<', 'ro<']:
+        rolls = (Model.query
+                .filter_by(dice_total=parsed_input['num'])
+                .filter_by(side_count=parsed_input['sides'])
+                .filter_by(roll_type=parsed_input['r_mod'] + str(parsed_input['r_val']))
+                .all()
+        )
+
+    else:
+        rolls = (Model.query
+                .filter_by(dice_total=parsed_input['num'])
+                .filter_by(side_count=parsed_input['sides'])
+                .filter_by(roll_type='base')
+                .all()
+        )
 
     return [{'pip_total': r.pip_total + parsed_input['const'], 'probability': r.probability} for r in rolls]
