b52743788529c248d5ebedaa2a35b6e27ad490c7
drb1001 <pi_anyone@hotmail.com>
2020-06-09 21:00:59 +0200

    Fix minimum possible value

--- src/read_data.py
+++ src/read_data.py
@@ -12,9 +12,10 @@ def read_data_from_db(app, db, Model, parsed_input):
         )
         return [{'pip_total': r.pip_total + parsed_input['const'],
                 'probability': r.base_prob}
-                for r in rolls]
+                for r in rolls ]
 
     elif parsed_input['r_mod'] in ['r<', 'ro<']:
+        min_roll = parsed_input['num'] * (parsed_input['r_val'] + 1) if parsed_input['r_mod'] == 'r<' else parsed_input['num']
         rolls = (Model.query
                 .filter_by(dice_total=parsed_input['num'])
                 .filter_by(side_count=parsed_input['sides'])
@@ -24,9 +25,10 @@ def read_data_from_db(app, db, Model, parsed_input):
         )
         return [{'pip_total': r.pip_total + parsed_input['const'],
                 'probability': r.get_r_data(parsed_input['r_mod'], parsed_input['r_val'])}
-                for r in rolls]
+                for r in rolls if r.pip_total >= min_roll]
 
     else:
+        # regular old roll
         rolls = (Model.query
                 .filter_by(dice_total=parsed_input['num'])
                 .filter_by(side_count=parsed_input['sides'])
